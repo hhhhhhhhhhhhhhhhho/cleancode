d0f2c441e607bc0e0cce6db1602a5c621f78d95b
proggg <xowjdpk@gmail.com>
2024-12-11 16:15:49 +0900

    .

---
+++ code/2. Meaningful nameing/meaningful_division.py
@@ -0,0 +1,12 @@
+## 함수이름만 봐도 무슨말인지 알 것 같다.
+
+def getActiveAccount():
+    pass
+def getActiveAccounts():
+    pass
+def getActiveAccountInfo():
+    pass
+
+
+### 명확한 관례가 없다면 이따위로 구분하기 힘들다. 
+### 처음 읽는 사람들도 차이를 알수 있도록 이름을 지어라.
